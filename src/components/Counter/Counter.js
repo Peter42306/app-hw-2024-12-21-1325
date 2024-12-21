b30831bc46a963ff644e26d8302782aab1ed6237
Petr Zalizko <pzalizko@gmail.com>
2024-12-21 13:35:47 +0200

    Created

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Counter.css';
+
+const Counter = () => (
+  <div className="Counter">
+    Counter Component
+  </div>
+);
+
+Counter.propTypes = {};
+
+Counter.defaultProps = {};
+
+export default Counter;
