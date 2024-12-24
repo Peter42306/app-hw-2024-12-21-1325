0b9899ee2e8b0fa6bc1a0c130342807e822eeb4b
Petr Zalizko <pzalizko@gmail.com>
2024-12-24 14:02:39 +0200

    Added component Form7

---
+++ src/components/Form7/Form7.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Form7.css';
+
+const Form7 = () => (
+  <div className="Form7">
+    Form7 Component
+  </div>
+);
+
+Form7.propTypes = {};
+
+Form7.defaultProps = {};
+
+export default Form7;
